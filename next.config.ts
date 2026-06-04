7bc2e847165111df22983e8e6e612281cda8db87
youzam136-sudo <youzam136@gmail.com>
2026-06-04 17:18:38 +0900

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     dangerouslyAllowLocalIP: true,
     remotePatterns: [
       { protocol: "https", hostname: "**" },
