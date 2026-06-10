bd3d62e4473ad101f076b867d4e16cf8fb8bd910
youzam136-sudo <youzam136@gmail.com>
2026-06-11 04:48:38 +0900

    fix: 상세페이지 수정

--- lib/api.ts
+++ lib/api.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
-  headers: {
-    Authorization: `Bearer ${process.env.BACKEND_API_TOKEN}`,
-  },
+  headers: process.env.BACKEND_API_TOKEN
+    ? { Authorization: `Bearer ${process.env.BACKEND_API_TOKEN}` }
+    : {},
 });
 
 export default api;
